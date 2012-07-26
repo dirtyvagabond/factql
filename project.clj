ce97ffb5b98c662dbf5fdfb7a3b12559ddb3268e
Aaron Crow <dirtyvagabond@yahoo.com>
2012-07-26 11:33:42 -0700

    Merge in progress that was made in pretty-ql. pretty-ql will now be considered deprecated in favor of factql.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject factql "1.0.1"
+  :url "https://github.com/dirtyvagabond/factql"
+  :description "A Clojure DSL for Factual's API"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [factual/factual-clojure-driver "1.3.1"]])
